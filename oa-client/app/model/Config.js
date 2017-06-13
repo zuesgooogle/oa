57ef13baa48419d93f192a6183ae4633d4474232
zuesgooogle <zeusgooogle@gmail.com>
2017-06-13 10:30:09 +0800

    change ws port

--- oa-client/app/model/Config.js
+++ oa-client/app/model/Config.js
@@ -1,6 +1,6 @@
 Ext.define('oa.config.Config', {
     singleton: true,
 
-    BASE_URL: 'http://47.93.85.206:9090/ws/'
-
+    // BASE_URL: 'http://47.93.85.206:9090/ws/'
+    BASE_URL: 'http://127.0.0.1:9090/ws/'
 });
