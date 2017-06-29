1a838cb1e89d8ed4b733189bec14d4028a3e0515
zuesgooogle <zeusgooogle@gmail.com>
2017-06-29 14:36:07 +0800

    change tableName

--- oa-sql/sql-init.sql
+++ oa-sql/sql-init.sql
@@ -1,39 +1,9 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : 127.0.0.1
-Source Server Version : 50716
-Source Host           : localhost:3306
-Source Database       : jinbao_second
-
-Target Server Type    : MYSQL
-Target Server Version : 50716
-File Encoding         : 65001
-
-Date: 2017-06-06 19:13:31
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : 127.0.0.1
-Source Server Version : 50716
-Source Host           : localhost:3306
-Source Database       : jinbao-second
-
-Target Server Type    : MYSQL
-Target Server Version : 50716
-File Encoding         : 65001
-
-Date: 2017-06-28 22:04:03
-*/
 
 -- ----------------------------
--- Table structure for cost_subject
+-- Table structure for oa_cost_subject
 -- ----------------------------
-DROP TABLE IF EXISTS `cost_subject`;
-CREATE TABLE `cost_subject` (
+DROP TABLE IF EXISTS `oa_cost_subject`;
+CREATE TABLE `oa_cost_subject` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) DEFAULT NULL COMMENT '上级科目',
   `name` varchar(64) NOT NULL,
@@ -41,20 +11,21 @@ CREATE TABLE `cost_subject` (
   `create_time` datetime NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='成本科目表';
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='成本科目表';
 
 -- ----------------------------
--- Records of cost_subject
+-- Records of oa_cost_subject
 -- ----------------------------
-INSERT INTO `cost_subject` VALUES ('1', null, 'Root', '0', '2017-06-28 21:13:00', '2017-06-28 21:13:04');
-INSERT INTO `cost_subject` VALUES ('2', '1', '置地公司', '1', '2017-06-28 21:13:18', '2017-06-28 21:13:22');
-INSERT INTO `cost_subject` VALUES ('3', '1', '置业公司', '2', '2017-06-28 21:13:35', '2017-06-28 21:13:38');
+INSERT INTO `oa_cost_subject` VALUES ('1', null, 'Root', '0', '2017-06-28 21:13:00', '2017-06-28 21:13:04');
+INSERT INTO `oa_cost_subject` VALUES ('2', '1', '置地公司', '1', '2017-06-28 21:13:18', '2017-06-28 21:13:22');
+INSERT INTO `oa_cost_subject` VALUES ('3', '1', '置业公司', '2', '2017-06-28 21:13:35', '2017-06-28 21:13:38');
+INSERT INTO `oa_cost_subject` VALUES ('4', '2', '11111', '1', '2017-06-28 22:33:51', '2017-06-28 22:33:51');
 
 -- ----------------------------
--- Table structure for department
+-- Table structure for oa_department
 -- ----------------------------
-DROP TABLE IF EXISTS `department`;
-CREATE TABLE `department` (
+DROP TABLE IF EXISTS `oa_department`;
+CREATE TABLE `oa_department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) DEFAULT NULL COMMENT '上级部门',
   `name` varchar(64) NOT NULL,
@@ -62,20 +33,20 @@ CREATE TABLE `department` (
   `create_time` datetime NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='部门信息表';
+) ENGINE=InnoDB AUTO_INCREMENT=112 DEFAULT CHARSET=utf8 COMMENT='部门信息表';
 
 -- ----------------------------
--- Records of department
+-- Records of oa_department
 -- ----------------------------
-INSERT INTO `department` VALUES ('1', null, 'Root', '0', '2017-06-06 17:08:05', '2017-06-28 21:00:24');
-INSERT INTO `department` VALUES ('2', '1', '置地公司', '1', '2017-06-08 18:23:25', '2017-06-28 20:51:57');
-INSERT INTO `department` VALUES ('3', '1', '置业公司', '2', '2017-06-21 17:24:35', '2017-06-28 20:52:07');
+INSERT INTO `oa_department` VALUES ('1', null, 'Root', '0', '2017-06-06 17:08:05', '2017-06-28 21:00:24');
+INSERT INTO `oa_department` VALUES ('2', '1', '置地公司', '1', '2017-06-08 18:23:25', '2017-06-28 20:51:57');
+INSERT INTO `oa_department` VALUES ('3', '1', '置业公司', '2', '2017-06-21 17:24:35', '2017-06-28 20:52:07');
 
 -- ----------------------------
--- Table structure for ledger_zhidi
+-- Table structure for oa_ledger_zhidi
 -- ----------------------------
-DROP TABLE IF EXISTS `ledger_zhidi`;
-CREATE TABLE `ledger_zhidi` (
+DROP TABLE IF EXISTS `oa_ledger_zhidi`;
+CREATE TABLE `oa_ledger_zhidi` (
   `id` bigint(20) NOT NULL,
   `serial_number` varchar(20) NOT NULL COMMENT '序号',
   `area` varchar(32) NOT NULL COMMENT '片区',
@@ -94,14 +65,14 @@ CREATE TABLE `ledger_zhidi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Records of ledger_zhidi
+-- Records of oa_ledger_zhidi
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for ledger_zhiye
+-- Table structure for oa_ledger_zhiye
 -- ----------------------------
-DROP TABLE IF EXISTS `ledger_zhiye`;
-CREATE TABLE `ledger_zhiye` (
+DROP TABLE IF EXISTS `oa_ledger_zhiye`;
+CREATE TABLE `oa_ledger_zhiye` (
   `id` bigint(20) NOT NULL,
   `serial_number` varchar(20) NOT NULL COMMENT '序号',
   `area` varchar(32) NOT NULL COMMENT '片区',
@@ -118,14 +89,14 @@ CREATE TABLE `ledger_zhiye` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Records of ledger_zhiye
+-- Records of oa_ledger_zhiye
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for position
+-- Table structure for oa_position
 -- ----------------------------
-DROP TABLE IF EXISTS `position`;
-CREATE TABLE `position` (
+DROP TABLE IF EXISTS `oa_position`;
+CREATE TABLE `oa_position` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) DEFAULT NULL COMMENT '上级职位',
   `name` varchar(64) NOT NULL,
@@ -137,21 +108,17 @@ CREATE TABLE `position` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='职位信息表';
 
 -- ----------------------------
--- Records of position
+-- Records of oa_position
 -- ----------------------------
-INSERT INTO `position` VALUES ('1', null, 'Root', '', '0', '2017-06-28 21:13:00', '2017-06-28 21:13:04');
-INSERT INTO `position` VALUES ('2', '1', '置地公司', '', '1', '2017-06-28 21:13:18', '2017-06-28 21:13:22');
-INSERT INTO `position` VALUES ('3', '1', '置业公司', '', '2', '2017-06-28 21:13:35', '2017-06-28 21:13:38');
+INSERT INTO `oa_position` VALUES ('1', null, 'Root', '', '0', '2017-06-28 21:13:00', '2017-06-28 21:13:04');
+INSERT INTO `oa_position` VALUES ('2', '1', '置地公司', '', '1', '2017-06-28 21:13:18', '2017-06-28 21:13:22');
+INSERT INTO `oa_position` VALUES ('3', '1', '置业公司', '', '2', '2017-06-28 21:13:35', '2017-06-28 21:13:38');
 
 -- ----------------------------
--- Records of position
+-- Table structure for oa_resource
 -- ----------------------------
-
--- ----------------------------
--- Table structure for resource
--- ----------------------------
-DROP TABLE IF EXISTS `resource`;
-CREATE TABLE `resource` (
+DROP TABLE IF EXISTS `oa_resource`;
+CREATE TABLE `oa_resource` (
   `id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
   `code` varchar(64) NOT NULL,
@@ -162,14 +129,14 @@ CREATE TABLE `resource` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资源菜单表';
 
 -- ----------------------------
--- Records of resource
+-- Records of oa_resource
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for role
+-- Table structure for oa_role
 -- ----------------------------
-DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
+DROP TABLE IF EXISTS `oa_role`;
+CREATE TABLE `oa_role` (
   `id` int(11) NOT NULL,
   `name` varchar(64) NOT NULL,
   `deleted` tinyint(4) NOT NULL,
@@ -179,14 +146,14 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
 
 -- ----------------------------
--- Records of role
+-- Records of oa_role
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for oa_user
 -- ----------------------------
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+DROP TABLE IF EXISTS `oa_user`;
+CREATE TABLE `oa_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(32) NOT NULL,
   `password` varchar(256) NOT NULL,
@@ -198,6 +165,36 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
--- Records of user
+-- Records of oa_user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '18810863658', '$2a$10$fnxrymljcZQgRpMxaNZZ8.Xjgro9jSm7tFhC5IyG7a.5/IPMLqM.G', '1', '0', '2017-06-22 17:25:04', '2017-06-22 17:25:06');
+INSERT INTO `oa_user` VALUES ('1', '18810863658', '$2a$10$fnxrymljcZQgRpMxaNZZ8.Xjgro9jSm7tFhC5IyG7a.5/IPMLqM.G', '1', '0', '2017-06-22 17:25:04', '2017-06-22 17:25:06');
+
+-- ----------------------------
+-- Table structure for oa_user_department
+-- ----------------------------
+DROP TABLE IF EXISTS `oa_user_department`;
+CREATE TABLE `oa_user_department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of oa_user_department
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for oa_user_position
+-- ----------------------------
+DROP TABLE IF EXISTS `oa_user_position`;
+CREATE TABLE `oa_user_position` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of oa_user_position
+-- ----------------------------
